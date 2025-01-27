c353d784380bb90f1b581db90ca081025a96aedf
joao.valduga <joao.valduga@angellira.com.br>
2025-01-27 15:50:15 -0300

    now working with jest coverage 80%

--- jest.config.ts
+++ jest.config.ts
@@ -18,13 +18,13 @@ const config = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -43,7 +43,14 @@ const config = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      statements: 80, // 80% de cobertura de instruções
+      branches: 80, // 80% de cobertura de ramificações
+      functions: 80, // 80% de cobertura de funções
+      lines: 80, // 80% de cobertura de linhas
+    },
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
